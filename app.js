1a95191750790b9d7b2f810b70ecca462442556b
MauricioR998 <mauricio.rivas98@hotmail.com>
2021-04-29 19:50:35 -0500

    Express Sessions Part 2

--- app.js
+++ app.js
@@ -17,13 +17,17 @@ const mongoose = require("mongoose");
 const Dishes = require("./models/dishes");
 
 const url = "mongodb://localhost:27017/conFusion";
-const connect = mongoose.connect(url);
+
+const options = { useMongoClient: true };
+
+mongoose.Promise = require("bluebird");
+const connect = mongoose.connect(url, options);
 
 connect.then(
   (db) => {
     console.log("Connected correctly to the server");
   },
-  (errr) => {
+  (err) => {
     console.log(err);
   }
 );
@@ -49,45 +53,24 @@ app.use(
   })
 );
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
+    var err = new Error("You are not authenticated!");
 
-    if (!authHeader) {
-      var err = new Error("You are not authenticated!");
-
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-
-    var username = auth[0];
-    var password = auth[1];
-
-    if (username == "admin" && password == "password") {
-      req.session.user = "admin";
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
+    err.status = 401;
+    return next(err);
   } else {
-    if (req.session.user == "admin") {
+    if (req.session.user == "authenticated") {
       next();
     } else {
       var err = new Error("You are not authenticated!");
 
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -96,8 +79,6 @@ function auth(req, res, next) {
 app.use(auth);
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
