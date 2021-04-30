667a30f1f169587c3d635f0f9838698d5826103c
MauricioR998 <mauricio.rivas98@hotmail.com>
2021-04-29 23:30:03 -0500

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -15,6 +16,7 @@ var leaderRouter = require("./routes/leaderRouter");
 const mongoose = require("mongoose");
 
 const Dishes = require("./models/dishes");
+const authenticate = require("./authenticate");
 
 const url = "mongodb://localhost:27017/conFusion";
 
@@ -53,26 +55,20 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
-
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error("You are not authenticated!");
 
-    err.status = 401;
+    err.status = 403;
     return next(err);
   } else {
-    if (req.session.user == "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
